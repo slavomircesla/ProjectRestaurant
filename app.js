47425c44afef20f56a6706100b63d6e9adbfa564
slavomircesla <32729870+slavomircesla@users.noreply.github.com>
2018-05-22 14:00:32 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -47,18 +47,57 @@ app.post('/drink',cors(),function(req,res){
 	
       
 	res.send(rows);
-	console.log(rows);
-			
-		
+	console.log(rows);		
 	});
 });
 
+app.post('/send', (req, res) => {
+	const output = `
+	  <p>You have a new contact request</p>
+	  <h3>Contact Details</h3>
+	  <ul>  
+		<li>Name: ${req.body.name}</li>
+		<li>Username: ${req.body.username}</li>
+		<li>Email: ${req.body.email}</li>
+		<li>Phone: ${req.body.phone}</li>
+	  </ul>
+	`;
+  
+	// create reusable transporter object using the default SMTP transport
+	let transporter = nodemailer.createTransport({
+	  host: 'gmail.com',
+	  port: 465,
+	  secure: true, // true for 465, false for other ports
+	  auth: {
+		  user: 'slavomir.cesla@akademiasovy.sk', // generated ethereal user
+		  pass: 'Kosice2018'  // generated ethereal password
+	  },
+	  tls:{
+		rejectUnauthorized:false
+	  }
+	});
+  
+	// setup email data with unicode symbols
+	let mailOptions = {
+		from: 'slavomir.cesla@akademiasovy.sk', // sender address
+		to: 'dominik.cesla@gmail.com', // list of receivers
+		subject: 'Reservation', // Subject line
+		text: 'Reservation', // plain text body
+		html: output // html body
+	};
+  
+	// send mail with defined transport object
+	transporter.sendMail(mailOptions, (error, info) => {
+		if (error) {
+			return console.log(error);
+		}
+		console.log('Message sent: %s', info.messageId);   
+		console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  
+		res.render('contact', {msg:'Email has been sent'});
+	});
+	});
 
-app.post('send', (req, res) => {
-	console.log(req.body);
-} )
-
-
-http.listen(8080,function(){
+html.listen(8080,function(){
 	console.log("Connected & Listen to port 8080");
 });
